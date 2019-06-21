16795db21a93a7b6f6ac1da4f576be5871b7247a
Ari Bender <Ari.Bender@copart.com>
2019-06-21 13:33:42 -0500

    Fixed typo

--- whois_bulk.py
+++ whois_bulk.py
@@ -3,11 +3,11 @@ import subprocess
 
 f = open("whois_ip_list.txt","r")
 
-for ips in f.readlines():
+for ip in f.readlines():
 	time.sleep(1)
-	test = subprocess.Popen(["whois", ips.rstrip()], stdout=subprocess.PIPE)
+	test = subprocess.Popen(["whois", ip.rstrip()], stdout=subprocess.PIPE)
 	output = test.communicate()[0]
 	list_output =  (output.splitlines())
 	for x in list_output:
 		if ("OrgName") in x:
-			print ips.rstrip() + "   "+ x
+			print ip.rstrip() + "   "+ x
